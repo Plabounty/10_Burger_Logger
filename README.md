142c4b23664e1a903357485172c1d2695e3e98d8
Jake Fireside <labountypaul@icloud.com>
2020-12-22 17:37:19 -0600

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 10_Burger_Logger
+
+This is a burger logger which allows you to choose to eat or throw up a gross burger. you also have the option of creating a new burger and deciding whether it will be gross or yummy. You also have the option to delete the burger from the list of options.
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO cats (name) VALUES ('Molly');
-INSERT INTO cats (name) VALUES ('Charlie');
-INSERT INTO cats (name, sleepy) VALUES ('Poppy', true);
-INSERT INTO cats (name, sleepy) VALUES ('Oscar', true);
-INSERT INTO cats (name, sleepy) VALUES ('Smudge', true);
-INSERT INTO cats (name) VALUES ('Daisy');
+INSERT INTO burgers (name) VALUES ('Bacon');
+INSERT INTO burgers (name) VALUES ('Cheese');
+INSERT INTO burgers (name, gross) VALUES ('Chicken', true);
+INSERT INTO burgers (name, gross) VALUES ('Deer', true);
+INSERT INTO burgers (name, gross) VALUES ('Racoon', true);
+INSERT INTO burgers (name) VALUES ('Garden');
